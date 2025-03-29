74ecbaafd14cd963b805f74fbd492c034bcab7a5
Veeagony <159520361+Veeagony@users.noreply.github.com>
2025-03-29 16:09:35 +0800

    update

--- idiscipline/idiscipline/settings.py
+++ idiscipline/idiscipline/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'idiscipline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "myapp/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- idiscipline/myapp/views.py
+++ idiscipline/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("goodbye world")
+    return render(request, "myapp/DOlogin.html")
